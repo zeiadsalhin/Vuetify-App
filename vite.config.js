a429d0221c41b506ca1fe8d86e08fff3e725fce6
zeiadsalhin <zeyadali011@gmail.com>
2024-02-19 09:36:39 +0200

    v1

--- vite.config.js
+++ vite.config.js
@@ -9,44 +9,7 @@ export default defineConfig({
   plugins: [
     vue(),
     // vueJsx(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true,
-        type: 'module',
-      },
-      injectRegister: 'auto',
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "logo.svg"],
-      manifest: {
-        name: "Farm DashBoard",
-        short_name: "Farm Dashboard",
-        description: "Control your farm from your mobile phone",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "/img/icons/apple-touch-icon-76x76.png",
-            sizes: "76x76",
-            type: "image/png",
-          },
-          {
-            src: "/img/icons/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/img/icons/android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "/img/icons/android-chrome-maskable-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-      },
-    }),
+
   ],
   resolve: {
     alias: {
