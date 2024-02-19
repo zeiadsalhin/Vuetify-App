e50a84ce68d1924fa5698132714c59798c052392
zeiadsalhin <zeyadali011@gmail.com>
2024-02-19 10:06:05 +0200

    v1

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6e3354dc'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2d32k1rm3d"
+    "revision": "0.tvv1ps1u7s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
