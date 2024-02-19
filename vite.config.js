09a50b692a3fc3aea9c35c58769bcbd6b6e1c7f2
zeiadsalhin <zeyadali011@gmail.com>
2024-02-19 09:47:42 +0200

    v1

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 // import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
@@ -9,6 +10,43 @@ export default defineConfig({
   plugins: [
     vue(),
     // vueJsx(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      injectRegister: 'auto',
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "logo.svg"],
+      manifest: {
+        name: " Vuetify Application",
+        short_name: "Vuetify Application",
+        description: "The next Gen web app",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/img/icons/apple-touch-icon-76x76.png",
+            sizes: "76x76",
+            type: "image/png",
+          },
+          {
+            src: "/img/icons/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/img/icons/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/img/icons/android-chrome-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
 
   ],
   resolve: {
