f6b79b5c7169e917321301873e87c030896f1d95
zeiadsalhin <zeyadali011@gmail.com>
2024-02-19 09:25:03 +0200

    v1

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import './registerServiceWorker'
 
 const vuetify = createVuetify({
     components,
